3ab3c4ddeb85b8c761a53671ef321984a5ad9c48
L1nar <122251664+L1nar@users.noreply.github.com>
2024-01-19 16:39:16 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector('#btn');
+const input = document.querySelector('#btn');
+const list = document.querySelector('#list');
+
+btn.addEventListener('click', function(event) {
+    event.preventDefault();
+    console.log('TEST');
+})
